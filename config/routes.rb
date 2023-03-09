475b084fc19a1acf88fa3d572108795b73cbf533
peacethedominator <varchaswiaman14@gmail.com>
2023-03-09 16:54:54 +0530

    alpha  commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destory]
+  resources :articles #as all the routes are exposed
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
+end
